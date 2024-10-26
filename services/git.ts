1d506ffacfca388c6e2bc4d1a59cf5907234abdc
Baltukov Nomto <nomto_04@mail.ru>
2024-10-27 03:13:18 +0800

    testing new branch

--- services/git.ts
+++ services/git.ts
@@ -1,1 +1,8 @@
 const id: number = 999;
+//test code Only on TestBranch
+const pizzaName: string = "test";
+const age: number = 999;
+const isActive: boolean = true;
+const arr: number[] = [1, 2, 3];
+
+console.log(id, pizzaName, age, isActive, arr);
